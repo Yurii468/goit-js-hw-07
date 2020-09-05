19d3a7d4aa56215f2b7dd321ea93f06d3deb8af6
Yurii Sokolov <nintendowc3@gmail.com>
2020-09-05 05:26:37 +0300

    Modyl-7

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const selectByClass = document.querySelector('#categories');
+console.log(`В списке ${selectByClass.children.length} категории`);
+const titleOfList = Array.from(document.querySelectorAll('.item'));
+titleOfList.map(item =>
+  console.log(
+    `В категории "${item.children[0].innerText}" находится ${item.children[1].children.length} элементов`,
+  ),
+);
